d6926c3b46c8938d7676fb43a9f03edb76d236ec
Andrzej Graska <graskaandrzej@gmail.com>
2024-10-26 23:37:01 +0200

    Wreszcie...

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,30 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    std::unique_ptr< Resource > test;
+
+public:
+    ResourceManager() : test(std::make_unique< Resource >()) {}
+
+    ResourceManager(const ResourceManager& input1) : test(std::make_unique< Resource >(*input1.test))
+    {}
+    ~ResourceManager()
+    {
+        
+    }
+    double get() { return test->get(); }
+
+    ResourceManager& operator=(const ResourceManager& test_trudny)
+    {
+        test = std::make_unique< Resource >(*test_trudny.test);
+        return *this;
+    }
+    // Zad 3
+    ResourceManager(ResourceManager&& input1) : test(std::move(input1.test)){}
+    ResourceManager& operator=(ResourceManager&& test_bardzo_trudny)
+    {
+        test = std::move(test_bardzo_trudny.test);
+        return *this;
+    }
 };
